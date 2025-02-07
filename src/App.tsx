570a54655b297c3d6b7d97ebf6374b5bb0bbf9d5
Srivathsan Ram <sri5617@gmail.com>
2025-02-07 09:52:19 +0800

    v0.1

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import Page1 from './Page1';
+import Page2 from './Page2';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Page1 />} />
+        <Route path="/gotcha_ass" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Page1.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Page1() {
+    const navigate = useNavigate();
+    const [yesButtonStyle, setYesButtonStyle] = useState<React.CSSProperties>({
+        position: 'absolute',
+        backgroundColor: 'green',
+        bottom: `20%`,
+        left: `60%`,
+        width: '100px',
+        height: '40px'
+    });
+    const [noCounter, setNoCounter] = useState(0);
+
+    const handleNoClick = () => {
+        setYesButtonStyle(prev => ({
+            position: 'absolute',
+            backgroundColor: 'green',
+            bottom: `20%`,
+            left: `60%`,
+            width: `${prev.width ? parseInt(prev.width.toString()) + 10 : 110}px`,
+            height: `${prev.height ? parseInt(prev.height.toString()) + 10 : 50}px`
+        }));
+        setNoCounter(prev => prev + 1);
+    };
+
+    const handleYesClick = () => {
+        if (noCounter < 20) {
+            navigate('/page3');
+        }
+        else{
+            navigate('/gotcha_ass')
+        }
+    };
+
+    const noButtonStyle: React.CSSProperties = { width: '100px', height: '40px', position: 'absolute' ,backgroundColor: 'red', bottom: `20%`, left: `20%`,};
+
+    return (
+        <div>
+            <h1>Hi bestie</h1>
+            <p>Do you want to be my valentine?</p>  
+            <img src="src/assets/red_blue_pill.jpg" alt="black guy" style={{width: '90%', height: '80%', position:'absolute'}} />
+            
+            {noCounter < 20 ?
+                <div style={{ display: 'flex', gap: '1rem' }}>
+                    <button
+                        style={yesButtonStyle}
+                        onClick={handleYesClick}
+                    >
+                        Yes
+                    </button>
+                    <button
+                        style={noButtonStyle}
+                        onClick={handleNoClick}
+                    >
+                        No
+                    </button>
+                </div> :
+                <div style={{ display: 'flex', gap: '1rem' }}>
+                    <button
+                        style={yesButtonStyle}
+                        onClick={handleNoClick}
+                    >
+                        No
+                    </button>
+                    <button
+
+                        style={noButtonStyle}
+                        onClick={handleYesClick}
+                    >
+                        Yes
+                    </button>
+                </div>}
+        </div>
+    );
+}
